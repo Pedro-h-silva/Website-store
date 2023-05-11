a6fec6dfe275ba6ca5d364ff17b95379a4903752
Pedro Henrique <pedromlk164@gmail.com>
2023-05-11 13:35:23 -0300

    seventh commit

--- Produtos/products.js
+++ Produtos/products.js
@@ -1,3 +1,21 @@
-function AddCart() {
-  alert("evento funcionando");
+function buy() {
+  const name = document.getElementById("name-product").textContent;
+  const price = document.getElementById("price-product").textContent;
+  const size = document.getElementById(
+    "ProductSelect-product-template-option-0"
+  ).value;
+  const image = document.getElementById("image-product").src;
+
+  const product = {
+    name: name,
+    price: price,
+    size: size,
+    image: image,
+  };
+
+  const redirectUrl = `/Buynow/buy.html?product=${encodeURIComponent(
+    JSON.stringify(product)
+  )}`;
+  window.location.href = redirectUrl;
+  console.log(product);
 }
